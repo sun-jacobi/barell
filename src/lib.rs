b4e17b663bf4390956c92f4c1b588b6ccb7869b3
sun-jacobi <sun1011jacobi@gmail.com>
2023-09-09 21:55:07 +0900

    added remove, contain

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,36 @@
 #![no_std]
-#![feature(core_intrinsics)]
-
+#![feature(strict_provenance)]
 use core::ptr::NonNull;
 
-#[derive(Default)]
+#[derive(Default, Clone, Copy)]
 pub struct Node {
     next: Option<NonNull<Node>>,
+    prev: Option<NonNull<Node>>,
 }
 
-#[derive(Default)]
-pub struct BareLL {
+#[derive(Default, Clone, Copy)]
+pub struct BareList {
     head: Option<NonNull<Node>>,
 }
 
-impl BareLL {
+impl BareList {
     pub fn is_empty(&self) -> bool {
         self.head.is_none()
     }
 
     pub fn push(&mut self, mut node: NonNull<Node>) {
-        if let Some(head) = self.head {
+        if let Some(mut head) = self.head {
             unsafe {
                 node.as_mut().next = Some(head);
+                head.as_mut().prev = Some(node);
             }
         }
 
         self.head = Some(node);
+    }
+
+    pub fn head(&self) -> Option<NonNull<Node>> {
+        self.head
     }
 
     pub fn pop(&mut self) -> Option<NonNull<Node>> {
@@ -36,15 +41,50 @@ impl BareLL {
         }
         None
     }
+
+    pub fn remove(&self, addr: u64) -> bool {
+        let mut head = self.head;
+        while let Some(mut node) = head {
+            if node.addr().get() as u64 == addr {
+                unsafe {
+                    if let Some(mut prev) = node.as_mut().prev {
+                        prev.as_mut().next = node.as_mut().next;
+                    }
+
+                    if let Some(mut next) = node.as_mut().next {
+                        next.as_mut().prev = node.as_mut().prev;
+                    }
+                }
+
+                return true;
+            }
+
+            head = unsafe { node.as_ref().next };
+        }
+
+        false
+    }
+
+    pub fn contains(&self, addr: u64) -> bool {
+        let mut head = self.head;
+        while let Some(node) = head {
+            if node.addr().get() as u64 == addr {
+                return true;
+            }
+
+            head = unsafe { node.as_ref().next };
+        }
+
+        false
+    }
 }
 
 impl Node {
     pub fn new() -> Self {
-        Self { next: None }
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.next.is_none()
+        Self {
+            next: None,
+            prev: None,
+        }
     }
 
     pub fn from_addr(addr: u64) -> NonNull<Node> {
@@ -55,14 +95,13 @@ impl Node {
 
 #[cfg(test)]
 mod test {
+    use core::mem::size_of;
 
-    use core::intrinsics::size_of;
-
-    use crate::{BareLL, Node};
+    use crate::{BareList, Node};
 
     #[test]
     fn basic_test() {
-        let mut bare_ll = BareLL::default();
+        let mut bare_ll = BareList::default();
         let pool = [0u64; 4096];
         let addr0 = pool.as_ptr() as u64;
 
