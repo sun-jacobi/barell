8b1b9845ae7789eed39bbcf11b4d2333c69d5ddb
sun-jacobi <sun1011jacobi@gmail.com>
2023-09-09 22:28:42 +0900

    fixed some bugs

--- src/lib.rs
+++ src/lib.rs
@@ -34,21 +34,33 @@ impl BareList {
     }
 
     pub fn pop(&mut self) -> Option<NonNull<Node>> {
-        if let Some(head) = self.head {
-            self.head = unsafe { head.as_ref().next };
+        if let Some(mut head) = self.head {
+            unsafe {
+                if let Some(mut next) = head.as_mut().next {
+                    next.as_mut().prev = head.as_mut().prev;
+                }
+            }
+
+            self.head = unsafe { head.as_mut().next };
 
             return Some(head);
         }
         None
     }
 
-    pub fn remove(&self, addr: u64) -> bool {
+    pub fn remove(&mut self, addr: u64) -> bool {
         let mut head = self.head;
         while let Some(mut node) = head {
             if node.addr().get() as u64 == addr {
                 unsafe {
+                    if node.as_mut().prev.is_none() && node.as_mut().next.is_none() {
+                        self.head = None;
+                    }
+
                     if let Some(mut prev) = node.as_mut().prev {
                         prev.as_mut().next = node.as_mut().next;
+                    } else {
+                        self.head = node.as_mut().next;
                     }
 
                     if let Some(mut next) = node.as_mut().next {
@@ -59,7 +71,7 @@ impl BareList {
                 return true;
             }
 
-            head = unsafe { node.as_ref().next };
+            head = unsafe { node.as_mut().next };
         }
 
         false
@@ -104,15 +116,127 @@ mod test {
         let mut bare_ll = BareList::default();
         let pool = [0u64; 4096];
         let addr0 = pool.as_ptr() as u64;
+        let addr1 = addr0 + size_of::<Node>() as u64;
+        let addr2 = addr1 + size_of::<Node>() as u64;
 
         bare_ll.push(Node::from_addr(addr0));
 
+        bare_ll.push(Node::from_addr(addr1));
+
+        bare_ll.push(Node::from_addr(addr2));
+
+        assert_eq!(bare_ll.pop(), Some(Node::from_addr(addr2)));
+        assert_eq!(bare_ll.pop(), Some(Node::from_addr(addr1)));
+        assert_eq!(bare_ll.pop(), Some(Node::from_addr(addr0)));
+
+        assert!(bare_ll.is_empty());
+    }
+
+    #[test]
+    fn contain_remove_test() {
+        let mut bare_ll = BareList::default();
+        let pool = [0u64; 4096];
+        let addr0 = pool.as_ptr() as u64;
         let addr1 = addr0 + size_of::<Node>() as u64;
+        let addr2 = addr1 + size_of::<Node>() as u64;
+
+        bare_ll.push(Node::from_addr(addr0));
 
         bare_ll.push(Node::from_addr(addr1));
 
+        bare_ll.push(Node::from_addr(addr2));
+
+        assert!(bare_ll.contains(addr1));
+        assert!(bare_ll.remove(addr1));
+        assert!(!bare_ll.contains(addr1));
+
+        assert!(bare_ll.remove(addr0));
+        assert!(bare_ll.remove(addr2));
+
+        assert!(bare_ll.is_empty());
+    }
+
+    #[test]
+    fn contain_remove_test_2() {
+        let mut bare_ll = BareList::default();
+        let pool = [0u64; 4096];
+        let addr0 = pool.as_ptr() as u64;
+        let addr1 = addr0 + size_of::<Node>() as u64;
+        let addr2 = addr1 + size_of::<Node>() as u64;
+
+        bare_ll.push(Node::from_addr(addr0));
+
+        bare_ll.push(Node::from_addr(addr1));
+
+        bare_ll.push(Node::from_addr(addr2));
+
+        assert!(bare_ll.contains(addr0));
+        assert!(bare_ll.remove(addr0));
+        assert!(!bare_ll.contains(addr0));
+
+        assert!(bare_ll.contains(addr1));
+        assert!(bare_ll.remove(addr1));
+        assert!(!bare_ll.contains(addr1));
+
+        assert!(bare_ll.contains(addr2));
+        assert!(bare_ll.remove(addr2));
+        assert!(!bare_ll.contains(addr2));
+
+        assert!(bare_ll.is_empty());
+    }
+
+    #[test]
+    fn contain_remove_test_3() {
+        let mut bare_ll = BareList::default();
+        let pool = [0u64; 4096];
+        let addr0 = pool.as_ptr() as u64;
+        let addr1 = addr0 + size_of::<Node>() as u64;
+        let addr2 = addr1 + size_of::<Node>() as u64;
+
+        bare_ll.push(Node::from_addr(addr0));
+
+        bare_ll.push(Node::from_addr(addr1));
+
+        bare_ll.push(Node::from_addr(addr2));
+
+        assert!(bare_ll.contains(addr2));
+        assert!(bare_ll.remove(addr2));
+        assert!(!bare_ll.contains(addr2));
+
         assert_eq!(bare_ll.pop(), Some(Node::from_addr(addr1)));
-        assert_eq!(bare_ll.pop(), Some(Node::from_addr(addr0)));
+
+        assert!(bare_ll.contains(addr0));
+        assert!(bare_ll.remove(addr0));
+        assert!(!bare_ll.contains(addr0));
+
+        assert!(bare_ll.is_empty());
+    }
+
+    #[test]
+    fn contain_remove_test_4() {
+        let mut bare_ll = BareList::default();
+        let pool = [0u64; 4096];
+        let addr0 = pool.as_ptr() as u64;
+        let addr1 = addr0 + size_of::<Node>() as u64;
+        let addr2 = addr1 + size_of::<Node>() as u64;
+
+        bare_ll.push(Node::from_addr(addr0));
+
+        bare_ll.push(Node::from_addr(addr1));
+
+        bare_ll.push(Node::from_addr(addr2));
+
+        assert!(bare_ll.contains(addr2));
+        assert!(bare_ll.remove(addr2));
+        assert!(!bare_ll.contains(addr2));
+
+        assert!(bare_ll.contains(addr1));
+        assert!(bare_ll.remove(addr1));
+        assert!(!bare_ll.contains(addr1));
+
+        assert!(bare_ll.contains(addr0));
+        assert!(bare_ll.remove(addr0));
+        assert!(!bare_ll.contains(addr0));
 
         assert!(bare_ll.is_empty());
     }
